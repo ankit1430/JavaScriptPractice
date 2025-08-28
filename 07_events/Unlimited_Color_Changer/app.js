4cb0bdd8bdcbb7e3f5ebfb1b8bc6c039bcb0fcd2
Preet Thakur <preetthakur@192.168.1.3>
2025-08-29 01:53:02 +0530

    New Changes

---
+++ 07_events/Unlimited_Color_Changer/app.js
@@ -0,0 +1,32 @@
+//random color
+
+const randomColor = ()=>{
+    let hex = '0123456789ABCDEF'
+    let color = '#'
+    for(let i = 0 ;i<6;i++)
+    {
+        color += hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+} 
+
+let intervalId;
+const startChangingColor = ()=>{
+    if(!intervalId)
+    {
+        intervalId =  setInterval(changeColorBg,1000)
+    }
+
+    function changeColorBg()
+    {
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+const stopChangingColor = () =>{
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+document.querySelector('#start').addEventListener('click',startChangingColor)
+document.querySelector('#stop').addEventListener('click',stopChangingColor)
